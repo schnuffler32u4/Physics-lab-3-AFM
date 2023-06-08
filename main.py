12a3e7c7a59c7760b5bf875adc924c706ed6ceed
Calin <calin.ghizdavet@gmail.com>
2023-06-08 12:13:25 +0200

    Got a plot

--- main.py
+++ main.py
@@ -3,15 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def flatten(data, order=1, mask=[]):
+    data_out = np.copy(data)  # create copy of data
+    data_in = np.copy(data)
+    if np.any(mask):
+        data_in[mask] = np.nan
+    for idx, (out, line) in enumerate(zip(data_out, data_in)):
+        ix = np.isfinite(line)
+        
+        x = np.arange(len(line))
+        p = np.polyfit(x[ix], line[ix], order)  # fit data to polynomial
+        y = np.polyval(p, x)
+        data_out[idx] = out - y  # subtract fit from data
+    return data_out
 file = "Measurements/Carbon-nanotubes/Image00005.nid"
 afm = read(file)
 
 data = afm.data 
 #data = pd.read_csv("Measurements/Carbon-nanotubes/Carbonano1.csv")
-print(data)
+data = data[0]
 # We begin by making sure that the data starts from 0
 
-data = data - np.ones(len(data), len(data[0]))*np.amin(data)
-
-plt.imshow(luminance_array, cmap='gray')
+data = data - np.ones((len(data), len(data[0])))*np.amin(data)
+data = data/np.amax(data)
+data = flatten(data)
+plt.imshow(data, cmap='gray', vmin=0, vmax=1)
+plt.axis('off')  # To remove the axes
 plt.show()
+
+print(data)
