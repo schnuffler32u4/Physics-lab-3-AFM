6f359c6ca25c92661fbe6d5dc898bf4a29ace9c2
Calin <calin.ghizdavet@gmail.com>
2023-06-19 14:21:57 +0200

    Skidadle

--- averaging.py
+++ averaging.py
@@ -5,12 +5,11 @@ import pandas as pd
 # We want to define a function such that  it allows us to load a specific csv files and then it averages one of the columns that is also predefined in the function 
 
 
-
 def average(file, column):
-    averaging = np.array()
+    averaging = np.array([])
     for datafile in file:
         data = pd.read_csv(datafile)
         data.rename(columns={column:"column"},inplace=True) # this is done in order to allow for files whose columns might have weird names
         intermediate = np.array(data.column)
-        np.append(averaging, intermediate) 
+        averaging = np.append(averaging, intermediate) 
     return str(np.average(averaging)) + "±" + str(np.std(averaging))
